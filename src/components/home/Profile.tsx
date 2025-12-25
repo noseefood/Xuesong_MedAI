66b6ac6f20f24672f436a880c8312d353b99e725
noseefood <lixuesong00@hotmail.com>
2025-12-25 15:23:36 +0100

    test22

--- src/components/home/Profile.tsx
+++ src/components/home/Profile.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import Image from 'next/image';
+// import Image from 'next/image';
 import {
     EnvelopeIcon,
     AcademicCapIcon,
@@ -111,13 +111,20 @@ export default function Profile({ author, social, features, researchInterests }:
         >
             {/* Profile Image */}
             <div className="w-64 h-64 mx-auto mb-6 rounded-2xl overflow-hidden shadow-lg hover:shadow-xl transition-all duration-200 hover:scale-105">
-                <Image
+                {/* <Image
                     src={author.avatar}
                     alt={author.name}
                     width={256}
                     height={256}
                     className="w-full h-full object-cover object-[32%_center]"
                     priority
+                /> */}
+                <img
+                    src={author.avatar}
+                    alt={author.name}
+                    width={256}
+                    height={256}
+                    className="w-full h-full object-cover object-[32%_center]"
                 />
             </div>
 
