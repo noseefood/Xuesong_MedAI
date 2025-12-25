4c3185bf80537eb82ede93d02ffba94d217aacaf
Xuesong <48125952+noseefood@users.noreply.github.com>
2025-12-25 10:34:50 +0100

    Add basePath and assetPrefix to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+
+  basePath: "/Xuesong_MedAI",
+  assetPrefix: "/Xuesong_MedAI",
+  
   images: {
     unoptimized: true,
   },
